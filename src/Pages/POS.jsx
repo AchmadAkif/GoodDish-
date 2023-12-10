b42688687e11388d00f71375ebe36a64d9bb476b
achmadakif <achmadakif7@gmail.com>
2023-12-10 21:52:08 +0700

    data structure adjustment

--- src/Pages/POS.jsx
+++ src/Pages/POS.jsx
@@ -9,10 +9,16 @@ function POS() {
   const productData = useLoaderData()
 
   return (
-    <>
-      <h1 className="font-gilroyBold text-[17px] pl-[30px] mb-3">Soups</h1>
-      <Carousel productData={productData} />
-    </>
+    <div className="space-y-[20px]">
+      <div>
+        <h1 className="font-gilroyBold text-[18px] pl-[30px] mb-3">Soups</h1>
+        <Carousel productData={productData.soups} />
+      </div>
+      <div>
+        <h1 className="font-gilroyBold text-[18px] pl-[30px] mb-3">Salads</h1>
+        <Carousel productData={productData.salads} />
+      </div>
+    </div>
   )
 }
 
@@ -20,7 +26,7 @@ export default POS
 
 // Loader
 export const dataLoader = async () => {
-  const res = await fetch('http://localhost:3000/soups')
+  const res = await fetch('http://localhost:3000/products')
 
   return res.json()
 }
