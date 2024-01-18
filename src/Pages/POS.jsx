d532f81e4661d46cb9fde5548da5eabea2d1720d
achmadakif <achmadakif7@gmail.com>
2024-01-18 17:25:51 +0700

    added calculate total price and calculateTax functionality

--- src/Pages/POS.jsx
+++ src/Pages/POS.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 // Router
 import { useLoaderData } from "react-router-dom"
 import { useOutletContext } from "react-router-dom"
@@ -15,18 +15,43 @@ import { v4 as uuidv4 } from 'uuid';
 function POS() {
   const [drawerIsOpen, setDrawerIsOpen] = useOutletContext()
   const [productOnCart, setProductOnCart] = useState([])
-  
+  const [totalPrice, setTotalPrice] = useState()
+  const [subtotalPrice, setSubtotalPrice] = useState()
+
   const productData = useLoaderData()
   const onClose = () => {
     setDrawerIsOpen(false)
   }
 
+  useEffect(() => {
+    let total = 0
+    let sub = 0
+
+    const calculateTax = (totalAmount) => {
+      total += sub + (totalAmount * (5 / 100))
+      setTotalPrice(total)
+    }
+
+    productOnCart.forEach(product => {
+      sub += product.price
+    })
+    setSubtotalPrice(sub)
+    calculateTax(sub)
+
+  },[productOnCart])
 
   const handleAddToCart = (product) => {
+    // const productExist = productOnCart.find(data => data.name === product.name)
+
+    // if(productExist) {
+    //   return
+    // }
+     
     const productCopy = {
       id: uuidv4(),
       name: product.name,
-      price: product.price
+      price: product.price,
+      amount: 1
     }
 
     setProductOnCart([...productOnCart, productCopy])
@@ -47,7 +72,7 @@ function POS() {
         <h1 className="font-gilroyBold text-[18px] pl-[30px] mb-3">Salads</h1>
         <Carousel handleAddToCart={handleAddToCart} productData={productData.salads} />
       </div>
-      <CartDrawer onClose={onClose} drawerIsOpen={drawerIsOpen} productOnCart={productOnCart} handleRemoveProduct={handleRemoveProduct} />
+      <CartDrawer subtotalPrice={subtotalPrice} totalPrice={totalPrice} onClose={onClose} drawerIsOpen={drawerIsOpen} productOnCart={productOnCart} handleRemoveProduct={handleRemoveProduct} />
     </div>
   )
 }
