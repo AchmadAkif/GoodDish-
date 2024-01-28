6e85357cea4fed180edd5cc4e8f07e2c81ebdd10
achmadakif <achmadakif7@gmail.com>
2024-01-28 22:32:06 +0700

    minor style improvement

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -20,7 +20,7 @@ const Carousel = ({productData, handleAddToCart}) => {
     className: "center",
     infinite: true,
     centerPadding: "60px",
-    slidesToShow: 3,
+    slidesToShow: 4,
     swipeToSlide: true,
     afterChange: function(index) {
       console.log(
@@ -30,30 +30,20 @@ const Carousel = ({productData, handleAddToCart}) => {
   };
 
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#000",
-          colorBgContainer: "#fff"
-        },
-      }}
-    >
-      <Slider {...settings} className="bg-white p-5 rounded-[14px]">
-        {productData.map( product => (
-          <div key={product.id} className="flex flex-col space-y-3 px-5 py-6 bg-[#eaeaea] rounded-md">
-            <h1 className="font-gilroyBold">{product.name}</h1>
-            <p className="font-gilroyMed">${product.price}</p>
-            <div className="flex">
-              {/* <Button icon={<MinusOutlined />} className="bg-white" onClick={handleReduceAmount} />
-              <input min={0} defaultValue={0} type="number" className="max-w-[35px] text-center outline-none font-gilroyMed bg-[#eaeaea]" />
-              <Button icon={<PlusOutlined />} className="bg-white" onClick={handleAddAmount} /> */}
-              <Button onClick={() => onAdd(product)}>Add to Cart</Button>
-            </div>
+    <Slider {...settings} className="bg-white p-5 rounded-[14px]">
+      {productData.map( product => (
+        <div key={product.id} className="flex flex-col space-y-3 px-5 py-6 bg-[#eaeaea] rounded-md">
+          <h1 className="font-gilroyBold">{product.name}</h1>
+          <p className="font-gilroyMed">${product.price}</p>
+          <div className="flex">
+            {/* <Button icon={<MinusOutlined />} className="bg-white" onClick={handleReduceAmount} />
+            <input min={0} defaultValue={0} type="number" className="max-w-[35px] text-center outline-none font-gilroyMed bg-[#eaeaea]" />
+            <Button icon={<PlusOutlined />} className="bg-white" onClick={handleAddAmount} /> */}
+            <Button className="bg-[#ffffff] border-0 font-gilroyMed" onClick={() => onAdd(product)}>Add to Cart</Button>
           </div>
-        ))}
-      </Slider>
-    </ConfigProvider>
-
+        </div>
+      ))}
+    </Slider>
   )
 }
 
