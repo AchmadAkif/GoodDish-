af83acc54916bc7ee35ad2e0ebd53934c97bffd7
achmadakif <achmadakif7@gmail.com>
2024-02-22 21:45:08 +0700

    fetch custom hooks

---
+++ src/Hooks/useFetch.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetch(url)
+    // fetch('http://localhost:3000/products')
+    .then(res => {
+      if(!res.ok) {
+        if(res.status === 404) {
+          throw Error('URL doesnt exist')
+        }
+      }
+      console.log(res)
+      return res.json()
+    })
+    .then(data => {
+      setData(data)
+      setIsLoading(false)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+  },[url])
+
+  return { data, isLoading }
+}
+
+export default useFetch
