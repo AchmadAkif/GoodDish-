da36b6d1c35ae47bb080c4ca485fece24724ba07
achmadakif <achmadakif7@gmail.com>
2024-10-08 23:07:12 +0700

    feat: add toastify lib for notification

--- src/utils/toastify.js
+++ src/utils/toastify.js
@@ -1,12 +1,12 @@
 import { toast } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 const notifySuccess = (msg) => {
-  toast.success(msg, {autoClose: 1500});
-}
+  toast.success(msg, { autoClose: 1500 });
+};
 
 const notifyError = (msg) => {
-  toast.error(msg, {autoClose: 1500})
-}
+  toast.error(msg, { autoClose: 1500 });
+};
 
-export {notifySuccess, notifyError}
+export { notifySuccess, notifyError };
