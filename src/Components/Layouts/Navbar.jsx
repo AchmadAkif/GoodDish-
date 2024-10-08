140214904d418efab653f2d7da7af6565bc463bd
achmadakif <achmadakif7@gmail.com>
2024-10-08 23:13:01 +0700

    chore (refactor): adjust folder structure

---
+++ src/Components/Layouts/Navbar.jsx
@@ -0,0 +1,50 @@
+// Antd
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import { Badge, Button } from 'antd';
+
+function Navbar({
+  setSearchKeyword,
+  drawerIsOpen,
+  setDrawerIsOpen,
+  showCartBtn,
+  productOnCart,
+}) {
+  const handleClick = () => {
+    setDrawerIsOpen(!drawerIsOpen);
+  };
+
+  const handleSearch = (e) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  return (
+    <>
+      {showCartBtn ? (
+        <div className="flex px-5 justify-end items-center font-gilroyBold">
+          <div className="space-x-3">
+            <input
+              onChange={handleSearch}
+              type="text"
+              placeholder="Type to search"
+              className="h-[30px] outline-none bg-[#eaeaea] rounded-2xl px-3 font-gilroyMed"
+            />
+            <Badge count={productOnCart.length}>
+              <Button
+                className="bg-[#eaeaea] border-0 shadow-none"
+                shape="circle"
+                icon={<ShoppingCartOutlined />}
+                onClick={handleClick}
+              />
+            </Badge>
+          </div>
+        </div>
+      ) : (
+        <div className="flex px-5 items-center font-gilroyBold">
+          <h1 className="text-[18px]">Dashboard</h1>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/Containers/POS.jsx
@@ -0,0 +1,201 @@
+// React
+import { useEffect, useState } from 'react';
+
+// Component
+import Carousel from '../Components/DataDisplay/Carousel/Carousel.component';
+import CartDrawer from '../Components/DataDisplay/CardDrawer/CartDrawer.component';
+import LoadingSkeleton from '../Commons/Skeleton';
+
+// Hooks
+import { useOutletContext } from 'react-router-dom';
+import useFetch from '../Hooks/useFetch';
+
+function POS() {
+  useEffect(() => {
+    let total = 0;
+    let sub = 0;
+
+    const calculateTax = (totalAmount) => {
+      total += sub + totalAmount * (2 / 100);
+      setTotalPrice(total);
+    };
+
+    productOnCart.forEach((product) => {
+      sub += product.price * product.amount;
+    });
+
+    setSubtotalPrice(sub);
+    calculateTax(sub);
+  });
+
+  const [
+    searchKeyword,
+    drawerIsOpen,
+    setDrawerIsOpen,
+    productOnCart,
+    setProductOnCart,
+    revenue,
+    setRevenue,
+    itemSold,
+    setItemSold,
+    notifySuccess,
+  ] = useOutletContext();
+  const [totalPrice, setTotalPrice] = useState();
+  const [subtotalPrice, setSubtotalPrice] = useState();
+
+  // Fetch
+  const { data: productData, isLoading } = useFetch(
+    'https://good-dish-json-server.vercel.app/products',
+  );
+
+  const onClose = () => {
+    setDrawerIsOpen(false);
+  };
+
+  const handleAddToCart = (product) => {
+    let checkProductIsExist = productOnCart.find(
+      (data) => data.id === product.id,
+    );
+
+    if (checkProductIsExist) {
+      let newCart = [];
+      let newItem;
+
+      productOnCart.forEach((dataProduct) => {
+        if (dataProduct.id == product.id) {
+          newItem = {
+            ...dataProduct,
+            amount: dataProduct.amount + 1,
+          };
+
+          newCart.push(newItem);
+        } else {
+          newCart.push(dataProduct);
+        }
+      });
+
+      setProductOnCart(newCart);
+      // console.log(productOnCart)
+      // console.log(newCart)
+    } else {
+      const productCopy = {
+        ...product,
+        amount: 1,
+      };
+
+      setProductOnCart([...productOnCart, productCopy]);
+    }
+  };
+
+  const handleRemoveProduct = (id) => {
+    setProductOnCart(productOnCart.filter((product) => product.id !== id));
+  };
+
+  const handleAddAmount = (dataID) => {
+    let checkProductIsExist = productOnCart.find((data) => dataID === data.id);
+
+    if (checkProductIsExist) {
+      let newCart = [];
+      let newItem;
+
+      productOnCart.forEach((dataProduct) => {
+        if (dataProduct.id == dataID) {
+          newItem = {
+            ...dataProduct,
+            amount: dataProduct.amount + 1,
+          };
+
+          newCart.push(newItem);
+        } else {
+          newCart.push(dataProduct);
+        }
+      });
+
+      setProductOnCart(newCart);
+    }
+
+    // console.log(productOnCart)
+  };
+
+  const handleRemoveAmount = (dataID) => {
+    let checkProductIsExist = productOnCart.find((data) => dataID === data.id);
+
+    if (checkProductIsExist) {
+      let newCart = [];
+      let newItem;
+
+      productOnCart.forEach((dataProduct) => {
+        if (dataProduct.id == dataID) {
+          newItem = {
+            ...dataProduct,
+            amount: dataProduct.amount - 1,
+          };
+
+          newCart.push(newItem);
+        } else {
+          newCart.push(dataProduct);
+        }
+      });
+
+      setProductOnCart(newCart);
+    }
+  };
+
+  const handlePlaceOrder = () => {
+    let quantity = 0;
+
+    productOnCart.forEach((product) => {
+      quantity += product.amount;
+    });
+
+    notifySuccess('Order Success!');
+    setItemSold(itemSold + quantity);
+    setRevenue(revenue + subtotalPrice);
+    setProductOnCart([]);
+  };
+
+  return (
+    <div className="space-y-[20px]">
+      <div>
+        <h1 className="font-gilroyBold text-[18px] pl-[30px] mb-3">Soups</h1>
+        {isLoading && <LoadingSkeleton />}
+        {productData && (
+          <Carousel
+            searchKeyword={searchKeyword}
+            handleAddToCart={handleAddToCart}
+            productData={productData.soups}
+          />
+        )}
+      </div>
+      <div>
+        <h1 className="font-gilroyBold text-[18px] pl-[30px] mb-3">Salads</h1>
+        {isLoading && <LoadingSkeleton />}
+        {productData && (
+          <Carousel
+            searchKeyword={searchKeyword}
+            handleAddToCart={handleAddToCart}
+            productData={productData.salads}
+          />
+        )}
+      </div>
+      <CartDrawer
+        subtotalPrice={subtotalPrice}
+        totalPrice={totalPrice}
+        onClose={onClose}
+        drawerIsOpen={drawerIsOpen}
+        productOnCart={productOnCart}
+        onAddAmount={handleAddAmount}
+        onRemoveAmount={handleRemoveAmount}
+        onRemoveProduct={handleRemoveProduct}
+        onPlaceOrder={handlePlaceOrder}
+      // onAmountChange={handleAmountChange}
+      // amount={amount}
+      // setAmount={setAmount}
+      // revenue={revenue}
+      // setRevenue={setRevenue}
+      />
+    </div>
+  );
+}
+
+export default POS;
