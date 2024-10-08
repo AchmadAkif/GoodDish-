be61b4258a4f2d88aa4b82911c95eda2b5e0ba78
achmadakif <achmadakif7@gmail.com>
2024-10-08 22:11:51 +0700

    chore (refactor): separate some component and refactor some function

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,81 +1,25 @@
-// React
-// import React, { Component } from "react";
+import carouselStyle from '../Styles/carouselStyle';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-// Antd
-// import { PlusOutlined, MinusOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import ItemCard from './ItemCard';
 
-// React-Slick
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-
-const Carousel = ({searchKeyword, productData, handleAddToCart}) => {
-  
-  const onAdd = (product) => {
-    handleAddToCart(product)
-  }
-
-  const settings = {
-    className: "center",
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 4,
-    swipeToSlide: true,
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ],
-    afterChange: function(index) {
-      console.log(
-        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-      );
-    }
+const Carousel = ({ searchKeyword, productData, handleAddToCart }) => {
+  const style = carouselStyle;
+  const filteredData = () => {
+    return productData.filter(item => {
+      return searchKeyword.toLowerCase() === '' ? item : item.name.toLowerCase().includes(searchKeyword.toLowerCase());
+    });
   };
 
   return (
-    <Slider {...settings} className="bg-white p-5 rounded-[14px]">
-      { productData.filter((item) => {
-        return searchKeyword.toLowerCase() === ''
-        ? item
-        : item.name.toLowerCase().includes(searchKeyword)
-      }).map( product => (
-          <div key={product.id} className="!flex justify-between px-5 py-6 bg-[#eaeaea] rounded-md">
-            <div className="space-y-3 ">
-              <h1 className="font-gilroyBold">{product.name}</h1>
-              <p className="font-gilroyMed">${product.price}</p>
-              <Button className="bg-[#ffffff] border-0 font-gilroyMed" onClick={() => onAdd(product)}>Add to Cart</Button>
-            </div>
-            <div>
-              <img src={product.img} alt="" className="h-[146px] w-[115px] rounded-md" />
-            </div>
-          </div>
-        ))
-      }
+    <Slider {...style} className="bg-white p-5 rounded-[14px]">
+      {filteredData().map((product) => (
+        <ItemCard product={product} handleAddToCart={handleAddToCart} />
+      ))}
     </Slider>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
