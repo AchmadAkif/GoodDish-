266e79487dff71a444a3d0d7a4a8d3716aa70569
achmadakif <achmadakif7@gmail.com>
2024-10-08 23:11:24 +0700

    feat: add customHook for fetching data

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,33 +1,33 @@
-import { useEffect, useState } from "react"
-import { notifyError } from "../utils/toastify"
+import { useEffect, useState } from 'react';
+import { notifyError } from '../utils/toastify';
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch(url)
-    // fetch('http://localhost:3000/products')
-    .then(res => {
-      if(!res.ok) {
-        if(res.status === 404) {
-          throw Error('URL doesnt exist')
-        } 
-      }
-      console.log(res)
-      return res.json()
-    })
-    .then(data => {
-      setData(data)
-      setIsLoading(false)
-    })
-    .catch(err => {
-      notifyError("Failed to fetch")
-      console.log(err.message)
-    })
-  },[url])
+      // fetch('http://localhost:3000/products')
+      .then((res) => {
+        if (!res.ok) {
+          if (res.status === 404) {
+            throw Error('URL doesnt exist');
+          }
+        }
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        notifyError('Failed to fetch');
+        console.log(err.message);
+      });
+  }, [url]);
 
-  return { data, isLoading }
-}
+  return { data, isLoading };
+};
 
-export default useFetch
+export default useFetch;
