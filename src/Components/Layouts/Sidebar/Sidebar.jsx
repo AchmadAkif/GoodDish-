17e9232068656cb76384aa3fe276d872e2250cf1
achmadakif <achmadakif7@gmail.com>
2024-10-09 21:06:59 +0700

    chore: clean codebase

--- src/Components/Layouts/Sidebar/Sidebar.jsx
+++ src/Components/Layouts/Sidebar/Sidebar.jsx
@@ -14,12 +14,6 @@ const Sidebar = ({ navigate }) => {
         height: '100vh',
         background: '#000',
       }}
-      onBreakpoint={(broken) => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
     >
       <div className="h-[100vh] flex flex-col justify-between">
         <SidebarMenu navigate={navigate} />
